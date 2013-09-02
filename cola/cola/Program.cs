960a4b222ad389a9b50ed12cce3beddfe75787ee
Incorporado <incorporado@gmail.com>
2013-09-02 18:15:29 -0300

    subo hola mundo

---
+++ cola/cola/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hola mundo");
+            Console.In.ReadLine();
+        }
+    }
+}
