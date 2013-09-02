730a9417925c466d43e9d42efd9652de1513c179
Incorporado <incorporado@gmail.com>
2013-09-02 18:49:20 -0300

    subo la interfaz y una clase Lista para ir escribiendo los metodos

---
+++ cola/cola/ICola.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cola
+{
+    public interface ICola
+    {
+        bool IsEmpty { get; }
+        int Size {get; }
+        void Add(object item); // agregar un item.
+        object Top { get; } // retornar el primer item, lanzar exception si esta vacio.
+        void Remove(); // remover el primer item, lanzar exception si esta vacio.
+    }
+}
---
+++ cola/cola/Lista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace cola
+{
+    class Lista : ICola
+    {
+        bool IsEmpty { get; }
+        int Size { get; }
+        void Add(object item); // agregar un item.
+        object Top { get; } // retornar el primer item, lanzar exception si esta vacio.
+        void Remove(); // remover el primer item, lanzar exception si esta vacio.
+    }
+}
